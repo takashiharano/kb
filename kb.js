16c08085b6ca7467e696e766309d8ffac47ff223
Takashi Harano <takashiharano@users.noreply.github.com>
2024-06-30 23:06:28 +0900

    Fix URL link

--- kb.js
+++ kb.js
@@ -1320,6 +1320,8 @@ kb.drawData = function(data) {
   if (drawMode == '1') {
     contentBody = contentBody.replace(/&quot;/g, '"');
     contentBody = util.linkUrls(contentBody);
+    contentBody = contentBody.replace(/(<a href=.+?)&gt;/g, '$1"');
+    contentBody = contentBody.replace(/&gt;<\/a>/g, '</a>&gt;');
 
     var w = kb.linkDataUrl(contentBody, false, -1);
     contentBody = w.s;
